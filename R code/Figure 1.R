c1117f6fcc296e70d514f1a9c4115a85fde4c8e0
zyang1919 <152336611+zyang1919@users.noreply.github.com>
2023-12-05 11:06:12 -0600

    Add files via upload

---
+++ R code/Figure 1.R
@@ -0,0 +1,211 @@
+## clean history
+rm(list=ls())
+
+########### f_Q(t) ################
+## read data 
+dir_odfl <- "~/Desktop/Dissertation/ODFL/Simulation Revision/Data"
+
+## data set
+Yall_50 <- get(load(paste(dir_odfl, "Simulated 200 Response (Y) n=50.RData", sep = '/')))
+Uall_50 <- get(load(paste(dir_odfl, "Simulated 200 Response (Uall) n=50.RData", sep = '/')))
+Zall_50 <- get(load(paste(dir_odfl, "Simulated 200 Response (Zall) n=50.RData", sep = '/')))
+Xmall_50 <- get(load(paste(dir_odfl, "Simulated 200 Response (Xm) n=50.RData", sep = '/')))
+
+## result 
+dir_rst <- "~/Desktop/Dissertation/ODFL/Simulation Revision/Result"
+rst <- get(load(paste(dir_rst, "Rst.50sz.NB.BP.Qua.200sim.RData", sep = '/')))
+rst.bnp <- get(load(paste(dir_rst, "Rst.50sz.NB.Qua.BNP.200sim.RData", sep = '/')))
+
+## using second data set 
+Y <- Yall_50[[2]]
+Z_all <- Zall_50[[2]]
+Xm <- Xmall_50[[2]]
+n <- ncol(Y)
+theta.bp <- rst$theta_sim[[2]]
+theta.bp <- matrix(theta.bp, nrow = 3)
+eta.bp <- rst$eta[2]
+W.bp <- rst$W_sim[[2]]
+W.bp <- mean(W.bp)
+yhat <- theta.bp[1,] + theta.bp[2,]*Xm + theta.bp[3,]*(Xm)^2 + eta.bp*log(W.bp)
+
+
+## making plot
+plot(c(1), c(1), type='n', ylim=c(min(Y), max(Y)), xlim = c(min(Xm), max(Xm)),
+     xlab = "", ylab = "")
+cols <- rainbow(3, s=0.5)
+for(i in 1:n){
+  lines(unlist(Z_all[[i]])[,2], Y[,i], type='l', lwd=0.5, col = rgb(0.3,0.3,0.3))
+}
+lines(Xm, yhat, col = "#FF8080", lwd=4)
+
+f.bnp <- rst.bnp$f_sim[[2]]
+f.bnp <- matrix(f.bnp, 18)
+eta.bnp <- rst.bnp$eta[2]
+W.bnp <- rst.bnp$W_sim[[2]]
+W.bnp <- mean(W.bnp)
+yhat.bnp <- f.bnp + eta.bnp*log(W.bnp)
+lines(Xm, yhat.bnp, col = "#80FF80", lwd=4)
+
+## true line
+Wtrue <- rlnorm(1, 0, sqrt(2))
+ytrue <- 5 - 0.01*Xm - 0.05*(Xm)^2 + 1*log(Wtrue)
+lines(Xm, ytrue, lwd = 4, col="#8080FF")
+
+
+## sample size 100
+## data set
+Yall_100 <- get(load(paste(dir_odfl, "Simulated 200 Response (Y) n=100.RData", sep = '/')))
+Uall_100 <- get(load(paste(dir_odfl, "Simulated 200 Response (Uall) n=100.RData", sep = '/')))
+Zall_100 <- get(load(paste(dir_odfl, "Simulated 200 Response (Zall) n=100.RData", sep = '/')))
+Xmall_100 <- get(load(paste(dir_odfl, "Simulated 200 Response (Xm) n=100.RData", sep = '/')))
+
+## read data 
+## use the 2nd 
+Y <- Yall_100[[2]]
+Z_all <- Zall_100[[2]]
+Xm <- Xmall_100[[2]]
+n <- ncol(Y)
+
+
+## making plot
+plot(c(1), c(1), type='n', ylim=c(min(Y), max(Y)), xlim = c(min(Xm), max(Xm)),
+     xlab = "", ylab = "")
+
+for(i in 1:n){
+  lines(unlist(Z_all[[i]])[,2], Y[,i], type='l', lwd=0.5, col=rgb(0.3, 0.3, 0.3))
+}
+
+## read results
+rst.100 <- get(load(paste(dir_rst, "Rst.100sz.NB.BP.Qua.200sim.RData", sep = '/')))
+rst.bnp <- get(load(paste(dir_rst, "Rst.100sz.NB.Qua.BNP.200sim.RData", sep = '/')))
+
+
+theta.bp <- rst.100$theta_sim[[2]]
+theta.bp <- matrix(theta.bp, nrow = 3)
+eta.bp <- rst.100$eta[2]
+W.bp <- rst.100$W_sim[[2]]
+W.bp <- mean(W.bp)
+yhat <- theta.bp[1,] + theta.bp[2,]*Xm + theta.bp[3,]*(Xm)^2 + eta.bp*log(W.bp)
+
+lines(Xm, yhat, col = '#FF8080', lwd=4)
+
+f.bnp <- rst.bnp$f_sim[[2]]
+f.bnp <- matrix(f.bnp, 18)
+eta.bnp <- rst.bnp$eta[2]
+W.bnp <- rst.bnp$W_sim[[2]]
+W.bnp <- mean(W.bnp)
+yhat.bnp <- f.bnp + eta.bnp*log(W.bnp)
+lines(Xm, yhat.bnp, col = "#80FF80", lwd=4)
+
+## true line
+Wtrue <- rlnorm(1, 0, sqrt(2))
+ytrue <- 5 - 0.01*Xm - 0.05*(Xm)^2 + 1*log(Wtrue)
+lines(Xm, ytrue, lwd = 4, col="#8080FF")
+
+
+
+
+########### f_S(t) ################
+
+## read data 
+dir_sinuous <- "~/Desktop/Dissertation/ODFL/Simulation Revision/Data_Sinuous"
+dir_rst <- "~/Desktop/Dissertation/ODFL/Simulation Revision/Result Sinuous"
+
+## data set
+Yall_50 <- get(load(paste(dir_sinuous, "Sinuous Simulated 200 Response (Y) n=50.RData", sep = '/')))
+Uall_50 <- get(load(paste(dir_sinuous, "Sinuous Simulated 200 Response (Uall) n=50.RData", sep = '/')))
+Zall_50 <- get(load(paste(dir_sinuous, "Sinuous Simulated 200 Response (Zall) n=50.RData", sep = '/')))
+Xmall_50 <- get(load(paste(dir_sinuous, "Sinuous Simulated 200 Response (Xm) n=50.RData", sep = '/')))
+
+## result
+rst <- get(load(paste(dir_rst, "Rst.50sz.NB.BP.Sinuous.200sim.RData", sep = '/')))
+rst.bnp <- get(load(paste(dir_rst, "Rst.50sz.NB.BNP.Sinuous.200sim.RData", sep = '/')))
+
+## using first data set 
+Y <- Yall_50[[2]]
+Z_all <- Zall_50[[2]]
+Xm <- Xmall_50[[2]]
+n <- ncol(Y)
+theta.bp <- rst$theta_sim[[2]]
+theta.bp <- matrix(theta.bp, nrow = 4)
+eta.bp <- rst$eta[2]
+W.bp <- rst$W_sim[[2]]
+W.bp <- mean(W.bp)
+yhat <- theta.bp[1,] + theta.bp[2,]*Xm + theta.bp[3,]*(Xm)^2 + + theta.bp[4,]*(Xm)^3 + eta.bp*log(W.bp)
+
+## making plot
+plot(c(1), c(1), type='n', ylim=c(min(Y), max(Y)), xlim = c(min(Xm), max(Xm)),
+     xlab = "", ylab = "")
+
+for(i in 1:n){
+  lines(unlist(Z_all[[i]])[,2], Y[,i], type='l', lwd=0.5, col=rgb(0.3, 0.3, 0.3))
+}
+cols <- rainbow(3, s=0.5)
+lines(Xm, yhat, col = cols[1], lwd=4)
+
+f.bnp <- rst.bnp$f_sim[[2]]
+f.bnp <- matrix(f.bnp, 19)
+eta.bnp <- rst.bnp$eta[2]
+W.bnp <- rst.bnp$W_sim[[2]]
+W.bnp <- mean(W.bnp)
+yhat.bnp <- f.bnp + eta.bnp*log(W.bnp)
+lines(Xm, yhat.bnp, col = cols[2], lwd=4)
+
+## true
+Wtrue <- rlnorm(1, 0, sd=sqrt(2))
+ytrue <- 5*sin(pi*(Xm-10)/10)/(1+2*((Xm-10)/5)^2*(sign((Xm-10)/5)+1))+4+1*log(Wtrue)
+lines(Xm, ytrue, lwd = 4, col=cols[3])
+
+
+
+### n=100
+## data set
+Yall_100 <- get(load(paste(dir_sinuous, "Sinuous Simulated 200 Response (Y) n=100.RData", sep = '/')))
+Uall_100 <- get(load(paste(dir_sinuous, "Sinuous Simulated 200 Response (Uall) n=100.RData", sep = '/')))
+Zall_100 <- get(load(paste(dir_sinuous, "Sinuous Simulated 200 Response (Zall) n=100.RData", sep = '/')))
+Xmall_100 <- get(load(paste(dir_sinuous, "Sinuous Simulated 200 Response (Xm) n=100.RData", sep = '/')))
+
+## result
+rst <- get(load(paste(dir_rst, "Rst.100sz.NB.BP.Sinuous.200sim.RData", sep = '/')))
+rst.bnp <- get(load(paste(dir_rst, "Rst.100sz.NB.BNP.Sinuous.200sim.RData", sep = '/')))
+
+## using first data set 
+Y <- Yall_100[[2]]
+Z_all <- Zall_100[[2]]
+Xm <- Xmall_100[[2]]
+n <- ncol(Y)
+theta.bp <- rst$theta_sim[[2]]
+theta.bp <- matrix(theta.bp, nrow = 4)
+eta.bp <- rst$eta[2]
+W.bp <- rst$W_sim[[2]]
+W.bp <- mean(W.bp)
+yhat <- theta.bp[1,] + theta.bp[2,]*Xm + theta.bp[3,]*(Xm)^2 + + theta.bp[4,]*(Xm)^3 + eta.bp*log(W.bp)
+
+## making plot
+plot(c(1), c(1), type='n', ylim=c(min(Y), max(Y)), xlim = c(min(Xm), max(Xm)),
+     xlab = "", ylab = "")
+
+for(i in 1:n){
+  lines(unlist(Z_all[[i]])[,2], Y[,i], type='l', lwd=0.5, col=rgb(0.3, 0.3, 0.3))
+}
+cols <- rainbow(3, s=0.5)
+lines(Xm, yhat, col = cols[1], lwd=4)
+
+f.bnp <- rst.bnp$f_sim[[2]]
+f.bnp <- matrix(f.bnp, 19)
+eta.bnp <- rst.bnp$eta[2]
+W.bnp <- rst.bnp$W_sim[[2]]
+W.bnp <- mean(W.bnp)
+yhat.bnp <- f.bnp + eta.bnp*log(W.bnp)
+lines(Xm, yhat.bnp, col = cols[2], lwd=4)
+
+## true
+Wtrue <- rlnorm(1, 0, sd=sqrt(2))
+ytrue <- 5*sin(pi*(Xm-10)/10)/(1+2*((Xm-10)/5)^2*(sign((Xm-10)/5)+1))+4+1*log(Wtrue)
+lines(Xm, ytrue, lwd = 4, col=cols[3])
+
+
+
+
+
+
